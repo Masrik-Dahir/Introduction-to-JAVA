066fdeb87d4539848c4dceedebffbf518accfd17
Masrik-Dahir <69909265+Masrik-Dahir@users.noreply.github.com>
2020-12-05 13:19:48 -0500

    Update CustomerTests.java

--- src/Projects/Project6/CustomerTests.java
+++ src/Projects/Project6/CustomerTests.java
@@ -1,3 +1,15 @@
+/****************************************************************************
+ * Movie Tickets
+ ****************************************************************************
+ *  This program keep track of the customer and the movies tickets they purchased in an array
+ *  The option to choose from while deciding the ticket types, food, drink and discount is
+ *  listed on separate enums
+ *_____________________________________________________
+ * Masrik Dahir
+ * 5 Vovember 2020
+ * CMSC 255-C90
+ * Copyright 2020, Masrik Dahir, All Right Reserved
+ ****************************************************************************/
 package Projects.Project6;
 
 import Projects.Project6.Customer;
